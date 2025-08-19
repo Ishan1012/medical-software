5d570b592be4185b5b08af780cb06152faa991e7
Ishan1012 <ishandwivedi1210@gmail.com>
2025-08-19 15:04:35 +0530

    fix: minor bugs fixed

--- frontend/web/wellnest-website/app/register/doctor/page.js
+++ frontend/web/wellnest-website/app/register/doctor/page.js
@@ -3,7 +3,7 @@ import Header from '@/components/Header'
 import DoctorRegistration from '@/components/DoctorRegistration'
 import React from 'react'
 
-export default function page() {
+export default function Page() {
   return (
     <div>
         <Header />
