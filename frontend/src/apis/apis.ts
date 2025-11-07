d2278a39c79688d782eedbd5548d5044f9cd574a
Ishan1012 <ishandwivedi1210@gmail.com>
2025-11-07 15:02:53 +0530

    minor fix

--- frontend/src/apis/apis.ts
+++ frontend/src/apis/apis.ts
@@ -5,12 +5,12 @@ const baseURL = process.env.NEXT_PUBLIC_BACKEND_URI || "http://localhost:5000/ap
 
 const api = axios.create({ baseURL });
 
-export const signInApi = (signInRequest: SignInRequest) => api.post('/auth/signin', signInRequest);
+export const signInApi = (signInRequest: SignInRequest) => api.post('/signin', signInRequest);
 
-export const signUpApi = (signUpRequest: SignUpRequest) => api.post('/auth/signup', signUpRequest);
+export const signUpApi = (signUpRequest: SignUpRequest) => api.post('/signup', signUpRequest);
 
-export const signInByGoogleApi = (code: string, role: string) => api.post('/auth/signin/google', { code, role }, { headers: { 'Content-Type': 'application/json' } });
+export const signInByGoogleApi = (code: string, role: string) => api.post('/signin/google', { code, role }, { headers: { 'Content-Type': 'application/json' } });
 
-export const bookAppoinementApi = (appointment: Appointment) => api.post('/appointment/', appointment);
+export const bookAppoinementApi = (appointment: Appointment) => api.post('/appointment', appointment);
 
 export const smartConsultApi = (consult: FormDataConsult) => api.post('/consult', consult);
