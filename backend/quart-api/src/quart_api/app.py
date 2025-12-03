444b8cdc65b9387077a11ac946a5ef24e0aecd64
Ishan1012 <ishandwivedi1210@gmail.com>
2025-12-03 15:36:15 +0530

    added render.yaml and other udates for deployment of quart api

--- backend/quart-api/src/quart_api/app.py
+++ backend/quart-api/src/quart_api/app.py
@@ -1,9 +1,12 @@
 from quart import Quart
 from quart_api.routes.home import home_bp
 from quart_api.routes.predict import predict_bp
+from quart_cors import cors
 
 def create_app():
     app = Quart(__name__)
+
+    app = cors(app, allow_origin=["http://localhost:5000"])
 
     app.register_blueprint(home_bp)
     app.register_blueprint(predict_bp)
--- backend/quart-api/src/quart_api/main.py
+++ backend/quart-api/src/quart_api/main.py
@@ -3,4 +3,5 @@ from quart_api.app import create_app
 app = create_app()
 
 def run() -> None:
-    app.run(port=5050)
+    # app.run(port=5050)
+    app.run(host="0.0.0.0", port=10000)
